60d5f65d503a7831e7b0ad0ae7d8b1329c4a0f62
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-09-15 11:33:06 +0900

    #0 feat/ 초기 환경 설정
    iOS 환경 설정입니다.

---
+++ campick/campick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  campick
+//
+//  Created by Admin on 9/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ campick/campick/campickApp.swift
@@ -0,0 +1,17 @@
+//
+//  campickApp.swift
+//  campick
+//
+//  Created by Admin on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct campickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
