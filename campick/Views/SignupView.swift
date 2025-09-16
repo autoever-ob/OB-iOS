a0d4aa6638740a2a986149e23f9ceeab13ce63fb
Gumraze-git <galaxydh4110@gmail.com>
2025-09-16 11:41:59 +0900

    #2 feat(login): 로그인 페이지 UI 및 관련 컴포넌트 구현
    - 로그인 페이지 UI 구현
    - 재사용 컴포넌트 분리
        - `FormLabel`: 폼의 각 필드에 대한 라벨
        - `OutlinedInputField`: 텍스트 및 보안 입력 필드
        - `BrandFiledButtonStyle`: 공통 버튼 스타일
    - 추후 구현할 페이지 추가
        - `FindPasswordView`
        - `SignupView`

---
+++ campick/Views/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  campick
+//
+//  Created by Admin on 9/16/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text("회원가입 페이지")
+            .font(.title)
+            .fontWeight(.bold)
+            .padding()
+    }
+}
