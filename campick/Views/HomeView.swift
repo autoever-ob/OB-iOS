27ddcc7ff37adf438eb7c2000d93adbcb72cbf09
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-09-20 12:35:40 +0900

    #76 fix(home): NavigationStack 추가

--- campick/Views/HomeView.swift
+++ campick/Views/HomeView.swift
@@ -13,39 +13,43 @@ struct HomeView: View {
     
     
     var body: some View {
-        ZStack {
-            AppColors.brandBackground
-                .ignoresSafeArea()
-            VStack(spacing: 0) {
-                // 헤더
-                Header(showSlideMenu: $showSlideMenu)
-                // 컨텐츠 섹션
-                ScrollView {
-                    VStack(spacing: 24) {
-                        // 상단 배너
-                        TopBanner()
-                        // 매물 찾기
-                        FindVehicle()
-                        // 차량 종류
-                        VehicleCategory()
-                        // 추천 매물
-                        RecommendVehicle()
-                        // 하단 배너
-                        BottomBanner()
-                            .padding(.bottom,70)
+        NavigationStack{
+            ZStack {
+                AppColors.brandBackground
+                    .ignoresSafeArea()
+                VStack(spacing: 0) {
+                    // 헤더
+                    Header(showSlideMenu: $showSlideMenu)
+                    // 컨텐츠 섹션
+                    ScrollView {
+                        VStack(spacing: 24) {
+                            // 상단 배너
+                            TopBanner()
+                            // 매물 찾기
+                            FindVehicle()
+                            // 차량 종류
+                            VehicleCategory()
+                            // 추천 매물
+                            RecommendVehicle()
+                            // 하단 배너
+                            BottomBanner()
+                                .padding(.bottom,70)
+                        }
+                        .padding()
                     }
-                    .padding()
+    //                .safeAreaInset(edge: .bottom) {
+    //                    BottomTabBarView(currentSelection: .home)
+    //                }
                 }
-//                .safeAreaInset(edge: .bottom) {
-//                    BottomTabBarView(currentSelection: .home)
-//                }
+                // 슬라이드 메뉴
+                ProfileMenu(showSlideMenu: $showSlideMenu)
+    //                .allowsHitTesting(showSlideMenu)
             }
-            // 슬라이드 메뉴
-            ProfileMenu(showSlideMenu: $showSlideMenu)
+            .onAppear {
+                viewModel.connectWebSocket(userId: "1")
+            }
         }
-        .onAppear {
-            viewModel.connectWebSocket(userId: "1")
-        }
+        
     }
 }
 
