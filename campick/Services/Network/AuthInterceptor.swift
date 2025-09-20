2801aeaafbd1cb5cc329e20878416e3ba3e57174
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-09-20 15:26:38 +0900

    #78 fix(api): 토큰 제거

--- campick/Services/Network/AuthInterceptor.swift
+++ campick/Services/Network/AuthInterceptor.swift
@@ -32,7 +32,7 @@ final class AuthInterceptor: RequestInterceptor {
             if !isAuthEndpoint {
                 let token = TokenManager.shared.accessToken
                 if !token.isEmpty {
-                    request.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImlkIjoxMiwicm9sZSI6IlJPTEVfVVNFUiIsImlhdCI6MTc1ODMyNDUyMywiZXhwIjoxNzU4MzI2MzIzfQ.klisE0qi7ZrkSkDVNXEIk75DMihS9m9_OTT0HGE71Tk", forHTTPHeaderField: "Authorization")
+                    request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
                 }
             }
         }
